f433443e3f718a46e6c04e802ac14f27c4046a19
Angel Rocha <angel.jmrocha@gmail.com>
2025-02-28 00:06:42 -0300

    dia 04: listas

---
+++ dia_04/listas.py
@@ -0,0 +1,44 @@
+idades = [28, 42, 43, 35, 39, 28, 38]
+
+print(idades)
+
+teo = ["Teo", "Calvo", 32, True, "Casado", 2342.98]
+
+print(teo)
+
+type(teo)
+
+print(teo[2])
+
+print(teo[5])
+
+print(teo[0])
+
+print("soma idades:", sum(idades))
+
+print("média idades:", sum(idades) / len(idades))
+
+print("menor idade:", min(idades))
+
+teo = ["Teo Calvo", 32, True, "Casado", ["Estagiário", "ds jr", "ds pl", "ds sr", "head"], [1500, 4000, 4550, 6500, 10000], ["Ana", "Maria", "Claudia"]]
+
+len(teo)
+
+print(teo[6][0])
+
+print(teo[-1])
+
+print(teo[-1][-1])
+
+teo[0:4]
+
+teo[4][3:5]
+
+teo[4][-2:]
+
+salarios = teo[5]
+salarios[::-1]
+salarios[::2]
+
+
+
